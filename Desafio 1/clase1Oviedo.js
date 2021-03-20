b54b392a66f0b2ab3d17fa8ee3ba6a36e78dbc85
RafaelOviedo <oviedo.rp@gmail.com>
2021-03-20 18:02:49 -0300

    Agregado archivo .js que conecta con el html

---
+++ Desafio 1/clase1Oviedo.js
@@ -0,0 +1,7 @@
+let color1 = prompt("Ingresa un color");
+alert("Color elegido: " + color1);
+
+let color2 = prompt("Ingresa otro color");
+alert("Color elegido: " + color2);
+
+alert("Los colores elegidos son " + color1 + " " + "y" + " " + color2);
