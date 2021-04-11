ae4ad2e953f79e5805ba9e48adfb07aa1fd6c1e6
RafaelOviedo <oviedo.rp@gmail.com>
2021-04-11 11:08:56 -0300

    js de primera entrega de proyecto final
    Se agregaron las variables de los productos a elegir (hasta el momento), y se agrearon un par de funciones para darle interaccion a la nav bar en el movimiento del scroll, y para hacer aparecer la barra lateral del carrito de compra.

---
+++ Entrega de Proyecto Final/layoutsimulador.js
@@ -0,0 +1,39 @@
+// Variables de productos
+
+let pizzas = [
+    { name: "muzzarella", price: 490 },
+    { name: "napolitana", price: 530 },
+    { name: "fugazzeta", price: 500 },
+    { name: "jamon y morron", price: 530 },
+];
+
+let hamburguesas = [
+    { name: "simple", price: 420 },
+    { name: "doble", price: 490 },
+    { name: "cheddar", price: 550 },
+    { name: "doble cheddar", price: 610 },
+];
+
+let bebidas = [
+    { name: "coca lata", price: 60 },
+    { name: "7up lata", price: 60 },
+    { name: "fanta lata", price: 60 },
+    { name: "pepsi lata", price: 60 },
+];
+
+// Cambiando el estilo de la navbar en scroll
+
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("nav-bar");
+    navbar.classList.toggle("onScroll", window.scrollY > 0);
+});
+
+// funcionalidad del lateral bar
+
+function closeLateralBar() {
+    document.getElementById("lateral-bar").style.width = "0px";
+}
+
+function openLateralBar() {
+    document.getElementById("lateral-bar").style.width = "400px";
+}
